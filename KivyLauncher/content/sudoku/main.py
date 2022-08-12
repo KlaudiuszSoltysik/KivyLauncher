420e439a7b0b7bdbe50d77c2c0dd9f1e7be0a83c
KlaudiuszSoltysik <109976941+KlaudiuszSoltysik@users.noreply.github.com>
2022-08-12 21:21:27 +0200

    Add files via upload
    fixed  sudoku layout (without menu :( )

--- KivyLauncher/content/sudoku/main.py
+++ KivyLauncher/content/sudoku/main.py
@@ -1,9 +1,11 @@
+#naprawić buttony, dodać ekran końcowy i początkowy, wegenerować plansze, dodać rozwiązywanie planszy
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.graphics import Rectangle
 from kivy.graphics import Color
 from kivy.graphics.vertex_instructions import Line
@@ -15,7 +17,35 @@ from kivy.config import Config
 
 import random
 
-class Sudoku(RelativeLayout):
+
+class KivySudokuApp(App):
+    def build(self):
+        return Sudoku()
+
+
+class SmallGrid(GridLayout):
+    pass
+
+
+class BigGrid(GridLayout):
+    pass
+
+
+class Grid(AnchorLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        big_grid = BigGrid()
+        for i in range(0, 9):
+            small_grid = SmallGrid()
+            for j in range(0, 9):
+                small_grid.add_widget(SudokuButton(pos_x = i, pos_y = j))
+                
+            big_grid.add_widget(small_grid)
+        self.add_widget(big_grid)
+        
+        
+class Sudoku(BoxLayout):
     lines = []
     buttons = []
     board = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -30,44 +60,67 @@ class Sudoku(RelativeLayout):
     
     useable_values = numberList=[1, 2, 3, 4, 5, 6, 7, 8, 9]  
     
-    seconds = 0
     seconds_passed = 0
-    minutes = 0
     minutes_passed = 0
     
-    spacing = 0
-    spacing2 = 16
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
-        self.game_init()
+        self.orientation = 'vertical'
+        
+        self.title = Label(text = "KivySudoku",
+                           font_size = self.height * 0.8,
+                           font_name = "label_font.ttf",
+                           color = (0, 0.25, 0.3, 1),
+                           bold = True,
+                           size_hint = (1, 0.15))
+        
+        self.timer = Label(font_size = self.height * 0.5,
+                           font_name = "label_font.ttf",
+                           color = (0, 0.25, 0.3, 1),
+                           bold = True,
+                           size_hint = (1, 0.1))
+        
+        self.add_widget(self.title)
+        self.add_widget(self.timer)
+        self.add_widget(Grid())
+        
+        self.board_init()
         
         Clock.schedule_interval(self.update, 1/60)
     
     
-    def game_init(self):
-        self.board_init(self.board)
-        self.window_init()
-    
-    
-    def board_init(self, board):
+    def board_init(self):
         for i in range(0, 81):
-            row = i//9
+            row = int(i/9)
             col = i%9
-            if board[row][col] == 0:
+            if self.board[row][col] == 0:
                 random.shuffle(self.useable_values)      
                 for value in self.useable_values:
                     if self.value_check(row, col, value):
-                        board[row][col] = value
-                        if self.is_full(board):
+                        self.board[row][col] = value
+                        if self.is_full(self.board):
                             return True
                         else:
-                            if self.board_init(board):
+                            if self.board_init():
                                 return True
                 break
-        board[row][col] = 0             
+        self.board[row][col] = 0             
 
+
+    def solve_board(self):
+        for i in range(0, 81):
+            row = int(i/9)
+            col = i%9
+            if self.board[row][col] == 0:
+                for value in range(1,10):
+                    if self.value_check(row, col, value):
+                        self.board[row][col] == value
+                        self.solve_board()
+                        self.board[row][col] = 0
+                return False     
+            
 
     def is_full(self, board):
         for row in range(0, 9):
@@ -93,78 +146,11 @@ class Sudoku(RelativeLayout):
                     return False
         return True
 
-        
-    def window_init(self):
-        Window.size = (500, 600)
-        
-        with self.canvas:            
-            Color(0.1, 0.1, 0.1)
-            self.background = Rectangle(pos = self.pos, 
-                                        size = self.size) 
-            
-            Color(0.95, 0.95, 0.95)
-            for i in range(0, 20):
-                self.lines.append(Line())
-            
-            for i in range(0, 9):
-                button_list = []
-                for j in range(0, 9):
-                    button_list.append(SudokuButton(pos_x = i, pos_y = j))
-                self.buttons.append(button_list)
-                
-            self.title = Label(text = "Sudoku",
-                               color = (0, 0.35, 0.4, 1))
-            self.timer = Label(color = (0, 0.35, 0.4, 1))
-        
                 
     def update(self, dt):
         self.seconds_passed += dt
-        
-        self.calculate_spacing()    
+         
         self.update_time()
-        self.update_grid()
-        self.update_labels()
-        
-        
-    def update_grid(self):                    
-        self.background.size = self.size
-        self.update_lines()
-        self.update_buttons()
-        
-    
-    def update_lines(self):
-        for i in range(0, 10):
-            self.lines[i].points = [self.width/2 - self.spacing/2 + (i - 4) * self.spacing, 
-                                    0.8 * self.height/2 - 4.5 * self.spacing, 
-                                    self.width/2 - self.spacing/2 + (i - 4) * self.spacing,
-                                    0.8 * self.height/2 + 4.5 * self.spacing]
-            
-            if i == 0 or i == 3 or i == 6 or i == 9:
-                self.lines[i].width = dp(2)
-        
-        for i in range(0, 10):
-            self.lines[i + 10].points = [self.width/2 - 4.5 * self.spacing, 
-                                       0.8 * self.height/2 - self.spacing/2 + (i - 4) * self.spacing, 
-                                       self.width/2 + 4.5 * self.spacing,
-                                       0.8 * self.height/2 - self.spacing/2 + (i - 4) * self.spacing]
-            
-            if i == 0 or i == 3 or i == 6 or i == 9:
-                self.lines[i + 10].width = dp(2)
-    
-    
-    def update_buttons(self):
-        k = 0
-        for i in range(5, -4, -1):
-            l = 0 
-            for j in range(-4, 5):
-                self.buttons[l][k].size = (self.spacing - dp(self.spacing2), 
-                                        self.spacing - dp(self.spacing2))
-                
-                self.buttons[l][k].pos = (self.width/2 + (j - 0.5) * self.spacing + self.spacing2/2,
-                                       0.8 * self.height/2 + (i - 1.5) * self.spacing + self.spacing2/2)
-                self.buttons[l][k].text = str(self.board[l][k])
-                l +=1
-            k += 1
         
         
     def update_time(self):
@@ -172,30 +158,12 @@ class Sudoku(RelativeLayout):
             self.seconds_passed -= 60
             self.minutes_passed += 1
             
-        self.seconds = format(int(self.seconds_passed), '02')
-        self.minutes = format(self.minutes_passed, '02')
+        seconds = str(format(int(self.seconds_passed), '02'))
+        minutes = str(format(self.minutes_passed, '02'))
         
+        self.timer.text = "Time: " + minutes + ":" + seconds
         
-    def calculate_spacing(self):
-        if self.width > 0.8 * self.height:
-            self.spacing = int(0.8 * self.height/9)
-        else:
-            self.spacing = int(self.width/9)
-            
-            
-    def update_labels(self):
-        self.title.font_name = "label_font.ttf"
-        self.title.size = self.size
-        self.title.font_size = dp(40)
-        self.title.pos = (0, (self.height - 80)/2)
-        
-        self.timer.font_name = "label_font.ttf"
-        self.timer.size = self.size
-        self.timer.font_size = dp(30)
-        self.timer.pos = (0, (self.height - 170)/2)
-        self.timer.text = "Time: " + str(self.minutes) + ":" + str(self.seconds)
-        
-        
+               
 class SudokuButton(Button):
     def __init__(self, pos_x, pos_y, **kwargs):
         super().__init__(**kwargs)
@@ -207,12 +175,14 @@ class SudokuButton(Button):
     
         
     def on_press(self):
-        print("pressed")
         return super().on_press()
     
-                
-class KivySudokuApp(App):
-    pass
+    
+class MenuWidget(RelativeLayout):
+    def on_touch_down(self, touch):
+        if self.opacity == 0:
+            return False
+        return super().on_touch_down(touch)
 
 
 KivySudokuApp().run()
